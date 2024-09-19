2a02b5eab23f418ecc62331c22a064f01ea517b3
KarthikKashyap03 <ramasamudraphanees.k@northeastern.edu>
2024-09-18 20:10:01 -0400

    Initial commit

---
+++ Assignment_1/src/model/PersonProfile.java
@@ -0,0 +1,199 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author karthik
+ */
+public class PersonProfile {
+    
+    String firstname;
+    String lastname;
+    String email;
+    String driverslicensenumber;
+    String driverslicensestate;
+    String socialsecuritynumber;
+    String address1;
+    String address2;
+    String city;
+    String state;
+    String zip;
+    String telephonenumber;
+    String faxnumber;
+    String age;
+    String MemType;
+    String Pwt;
+    String gender;
+    String Ename;
+    String Phone;
+    String Enumber;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDriverslicensenumber() {
+        return driverslicensenumber;
+    }
+
+    public void setDriverslicensenumber(String driverslicensenumber) {
+        this.driverslicensenumber = driverslicensenumber;
+    }
+
+    public String getDriverslicensestate() {
+        return driverslicensestate;
+    }
+
+    public void setDriverslicensestate(String driverslicensestate) {
+        this.driverslicensestate = driverslicensestate;
+    }
+
+    public String getSocialsecuritynumber() {
+        return socialsecuritynumber;
+    }
+
+    public void setSocialsecuritynumber(String socialsecuritynumber) {
+        this.socialsecuritynumber = socialsecuritynumber;
+    }
+
+    public String getAddress1() {
+        return address1;
+    }
+
+    public void setAddress1(String address1) {
+        this.address1 = address1;
+    }
+
+    public String getAddress2() {
+        return address2;
+    }
+
+    public void setAddress2(String address2) {
+        this.address2 = address2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getTelephonenumber() {
+        return telephonenumber;
+    }
+
+    public void setTelephonenumber(String telephonenumber) {
+        this.telephonenumber = telephonenumber;
+    }
+
+    public String getFaxnumber() {
+        return faxnumber;
+    }
+
+    public void setFaxnumber(String faxnumber) {
+        this.faxnumber = faxnumber;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getMemType() {
+        return MemType;
+    }
+
+    public void setMemType(String MemType) {
+        this.MemType = MemType;
+    }
+
+    public String getPwt() {
+        return Pwt;
+    }
+
+    public void setPwt(String Pwt) {
+        this.Pwt = Pwt;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getEname() {
+        return Ename;
+    }
+
+    public void setEname(String Ename) {
+        this.Ename = Ename;
+    }
+
+    public String getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(String Phone) {
+        this.Phone = Phone;
+    }
+
+    public String getEnumber() {
+        return Enumber;
+    }
+
+    public void setEnumber(String Enumber) {
+        this.Enumber = Enumber;
+    }
+    
+
+   
+
+   
+    
+}
